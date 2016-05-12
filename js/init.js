f76641be4bed5a13bdde5bd22279c4aef72139a1
tkvern <tkvern@qq.com>
2016-05-12 23:48:30 +0800

    init qcut_wx template

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+    $('.button-collapse').sideNav({
+      edge: 'right',
+      closeOnClick: false
+    });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
